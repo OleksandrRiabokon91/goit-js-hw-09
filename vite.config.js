3deedae5eac0882adfb190801e90ac1e5808d8d0
Oleksandr Riabokon <riabokon.oleksandr@gmail.com>
2025-05-04 22:43:47 +0300

    reset vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
-import { glob } from 'glob';
 
 export default defineConfig(({ command }) => {
   return {
@@ -13,11 +14,35 @@ export default defineConfig(({ command }) => {
       sourcemap: true,
       rollupOptions: {
         input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
+        },
       },
       outDir: '../dist',
       emptyOutDir: true,
     },
-    plugins: [FullReload(['./src/*.html']), SortCss({ sort: 'mobile-first' })],
-    base: '/goit-js-hw-09/',
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      SortCss({
+        sort: 'mobile-first',
+      }),
+    ],
   };
 });
